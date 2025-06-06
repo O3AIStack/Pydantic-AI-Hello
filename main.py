92b3e82403be4b30b352073f2c8e352fe4eebd8b
EdPython <82699555+EdPython@users.noreply.github.com>
2025-06-06 09:45:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+# Hello World example from the Pydantic website (https://ai.pydantic.dev/#hello-world-example)
 
-print('test'
-      
-      )
+from pydantic_ai import Agent
+
+agent = Agent(
+    'google-gla:gemini-1.5-flash',
+    system_prompt='Be concise, reply with one sentence',
+)
+
+result = agent.run_sync('Where does "Hello World" come from?')
+print(result.output)
+
